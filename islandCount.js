65172b8c35ebedc4baa4d1a1a2097660570f2d4b
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-16 08:42:16 -0600

    Update islandCount.js

--- islandCount.js
+++ islandCount.js
@@ -1,7 +1,48 @@
+
+
 function countIslands (mapStr) {
   console.log(mapStr);
+
+  // find the x size
+  let xSize = mapStr.split('\n')[0].length;
+
+  //find the y size
+  let ySize = mapStr.split('\n').length;
+
+  // copy the string into a matrix
+  let matrix = [];
+  for(let y=0; y<ySize; y++) {
+    matrix[y] = [];
+    for(let x=0; x<xSize; x++) {
+      matrix[y][x] = mapStr.split('\n')[y][x];
+    }
+  }
+
+  console.log(matrix);
+
+  let cnt = 0;
+  for(let y=0; y<ySize; y++)
+    for(let x=0; x<xSize; x++)
+      count(x, y, '0');
+
+  console.log(matrix);
+
+  function count(x, y, color) {
+    if(matrix[y] && matrix[y][x]) {
+      if(matrix[y][x] !== color || matrix[y][x] === 'X')
+        return;
+
+      matrix[y][x] = 'X';
+      count(x, y+1, color);
+      count(x, y-1, color);
+      count(x-1, y, color);
+      count(x+1, y, color);
+    }
+  }
   
 }
+
+
 
 // start x=0, y=0
 
@@ -19,3 +60,13 @@ function countIslands (mapStr) {
 //     Perform Flood-fill (one step to the west of node, target-color, replacement-color).
 //     Perform Flood-fill (one step to the east of node, target-color, replacement-color).
 // 5. Return.
+
+
+
+var string = ('..000.\n'+
+'..0...\n'+
+'..0.0.\n'+
+'..0...\n'+
+'..000.');
+
+console.log(countIslands(string));
